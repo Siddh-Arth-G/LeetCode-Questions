d7986f23dc3421a7c3868143b656b06e0bc46486
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-08-31 20:41:07 +0530

    Time: 2 ms (75.89%), Space: 44.7 MB (12.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,16 +1,18 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         int min = Integer.MAX_VALUE;
-        for(int i=0; i<prices.length-1; i++){
-            for(int j=i+1; j<prices.length; j++){
-                if(prices[i]+prices[j] <= money){
-                    min = Math.min(min,(prices[i]+prices[j]));
+        for (int i=0;i<prices.length;i++) {
+            for (int j=0;j<prices.length;j++) {
+                int sum = 0;
+                if (i != j) {
+                    sum = prices[i]+prices[j];
+                    if (sum < min && sum <= money)
+                    min = sum;
                 }
             }
         }
-        if(min != Integer.MAX_VALUE){
-            return money - min;
-        }
+        if (min != 2147483647)
+        return money-min;
         return money;
     }
 }
