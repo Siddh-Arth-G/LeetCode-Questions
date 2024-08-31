ad11bae405b0dc8dc4dc204080c59650ae796b2f
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-08-31 22:44:53 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (82.99%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,12 +1,21 @@
 class Solution {
-    public int mostWordsFound(String[] sentences) {
-        int max = 0;
-        for(String sentence:sentences){
-            String[] words = sentence.split(" ");
-            if(words.length > max){
-                max = words.length;
-            }
+    public static int getWhiteSpaces(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == ' ')
+                c++;
         }
-        return max;
+        return c;
+    }
+    public static int mostWordsFound(String[] sentences) {
+        int n = sentences.length;
+        int max=-1;
+        int temp=0;
+        for(int i=0; i<n;i++) {
+            temp = getWhiteSpaces(sentences[i]);
+            if(temp>max)
+                max=temp;
+        }
+        return max+1;
     }
 }
