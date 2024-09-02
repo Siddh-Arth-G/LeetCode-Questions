0f23ae3715699a406341eaa041c39e69de13e604
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-09-02 10:25:52 +0530

    Time: 2 ms (72.59%), Space: 60.5 MB (8.89%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -16,18 +16,42 @@ class Solution {
         //     }
         // }
         // return -1;
-        int ans = 0;
-        outer:
-        while(true){
-            for(int i=0; i<chalk.length; i++){
-                if(k>=chalk[i]){
-                    k -= chalk[i];
-                }else{
-                    ans = i;
-                    break outer;
-                }
+        
+        
+        
+
+        // Second Solution
+        
+        // int ans = 0;
+        // outer:
+        // while(true){
+        //     for(int i=0; i<chalk.length; i++){
+        //         if(k>=chalk[i]){
+        //             k -= chalk[i];
+        //         }else{
+        //             ans = i;
+        //             break outer;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        
+        // Third solution
+        long sum = 0;
+        for (int i = 0; i < chalk.length; i++) {
+            sum += chalk[i];
+            if (sum > k) {
+                break;
             }
         }
-        return ans;
+        k = k % (int) sum;
+        for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k = k - chalk[i];
+        }
+        return 0;
     }
 }
