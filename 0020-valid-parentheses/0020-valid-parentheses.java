2c59550f041a38c318314e684e03e9b146ef92c7
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-09-18 14:46:04 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (36.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> ad = new ArrayDeque<>();
+//         ArrayDeque<Character> ad = new ArrayDeque<>();
         
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
-                ad.push(s.charAt(i));
-            }else{
-                if(ad.isEmpty()){
-                    return false;
-                }else if((s.charAt(i) == ')' && ad.pop() == '(') || 
-                         (s.charAt(i) == ']' && ad.pop() == '[') || 
-                         (s.charAt(i) == '}' && ad.pop() == '{')){
-                            continue;
-                }else{
-                    return false;
-                }
+//         for(int i=0; i<s.length(); i++){
+//             if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+//                 ad.push(s.charAt(i));
+//             }else{
+//                 if(ad.isEmpty()){
+//                     return false;
+//                 }else if((s.charAt(i) == ')' && ad.pop() == '(') || 
+//                          (s.charAt(i) == ']' && ad.pop() == '[') || 
+//                          (s.charAt(i) == '}' && ad.pop() == '{')){
+//                             continue;
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//         }
+//         return (ad.isEmpty());
+        
+        char[] charray = new char[s.length()];
+        int index = -1;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                index = index + 1;
+                charray[index] = ')';
+            } else if (ch == '{') {
+                index = index + 1;
+                charray[index] = '}';
+            } else if (ch == '[') {
+                index = index + 1;
+                charray[index] = ']';
+            } else if (index == -1 || charray[index--] != ch) {
+                return false;
             }
         }
-        return (ad.isEmpty());
+        return index == -1;
     }
 }
