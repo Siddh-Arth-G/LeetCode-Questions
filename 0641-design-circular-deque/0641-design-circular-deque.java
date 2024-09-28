0173bb558e829d0b361e79738c0ed520583f5212
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-09-28 20:54:38 +0530

    Time: 4 ms (100.00%), Space: 44.7 MB (61.84%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.java
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -1,74 +1,137 @@
-class MyCircularDeque {
+// class MyCircularDeque {
     
-    static Deque<Integer> d;
-    static int size;
+//     static Deque<Integer> d;
+//     static int size;
 
+//     public MyCircularDeque(int k) {
+//         d = new LinkedList<>();
+//         size = k;
+//     }
+    
+//     public boolean insertFront(int value) {
+//         if(d.size() < size){
+//             d.addFirst(value);
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     public boolean insertLast(int value) {
+//         if(d.size() < size){
+//             d.addLast(value);
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     public boolean deleteFront() {
+//         if(d.size()>0){
+//             d.removeFirst();
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     public boolean deleteLast() {
+//         if(d.size()>0){
+//             d.removeLast();
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     public int getFront() {
+//         if(d.size() <= 0){
+//             return -1;
+//         }
+//         return d.getFirst();
+//     }
+    
+//     public int getRear() {
+//         if(d.size() <= 0){
+//             return -1;
+//         }
+//         return d.getLast();
+//     }
+    
+//     public boolean isEmpty() {
+//         if(d.size() == 0){
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     public boolean isFull() {
+//         if(d.size() == size){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+class MyCircularDeque {
+    private int head, tail, size, n;
+    private final int[] a;
     public MyCircularDeque(int k) {
-        d = new LinkedList<>();
-        size = k;
+        a = new int[k];
+        n = a.length;
+        tail = 1;
     }
     
-    public boolean insertFront(int value) {
-        if(d.size() < size){
-            d.addFirst(value);
-            return true;
-        }
-        return false;
+    public boolean insertFront(int x) {
+        if (size == n) return false;
+        a[head = ++head % n] = x;
+        size++;
+        return true;
     }
     
-    public boolean insertLast(int value) {
-        if(d.size() < size){
-            d.addLast(value);
-            return true;
-        }
-        return false;
+    public boolean insertLast(int x) {
+        if (size == n) return false;
+        a[tail = (--tail + n) % n] = x;
+        size++;
+        return true;
     }
     
     public boolean deleteFront() {
-        if(d.size()>0){
-            d.removeFirst();
-            return true;
-        }
-        return false;
+        if (size == 0) return false;
+        head = (--head + n) % n;
+        size--;
+        return true;
     }
     
     public boolean deleteLast() {
-        if(d.size()>0){
-            d.removeLast();
-            return true;
-        }
-        return false;
+        if (size == 0) return false;
+        tail = ++tail % n;
+        size--;
+        return true;
     }
     
     public int getFront() {
-        if(d.size() <= 0){
-            return -1;
-        }
-        return d.getFirst();
+        return size == 0 ? -1 : a[head];
     }
     
     public int getRear() {
-        if(d.size() <= 0){
-            return -1;
-        }
-        return d.getLast();
+        return size == 0 ? -1 : a[tail];
     }
     
     public boolean isEmpty() {
-        if(d.size() == 0){
-            return true;
-        }
-        return false;
+        return size == 0;
     }
     
     public boolean isFull() {
-        if(d.size() == size){
-            return true;
-        }
-        return false;
+        return size == a.length;
     }
 }
-
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
  * MyCircularDeque obj = new MyCircularDeque(k);
