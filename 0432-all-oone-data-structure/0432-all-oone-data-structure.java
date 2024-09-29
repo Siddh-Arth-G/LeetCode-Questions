deaaa05cd5cf16ff0409ea817574c09fa9f71e7b
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-09-29 06:39:47 +0530

    Time: 58 ms (28.03%), Space: 61.5 MB (10.85%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -0,0 +1,58 @@
+class AllOne {
+    private final Map<String, Integer> stringCount;
+    private final TreeMap<Integer, Set<String>> bucketMap;
+    public AllOne() {
+        stringCount = new HashMap<>();
+        bucketMap = new TreeMap<>();
+    }
+    
+    public void inc(String key) {
+        int newCount = stringCount.merge(key, 1, Integer::sum);
+        removeFromOldBucket(key, newCount - 1);
+        addToNewBucket(key, newCount);
+    }
+    
+    public void dec(String key) {
+        int newCount = stringCount.merge(key, -1, Integer::sum);
+        if (newCount == 0) {
+            stringCount.remove(key);
+        }
+        removeFromOldBucket(key, newCount + 1);
+        addToNewBucket(key, newCount);
+    }
+
+    private void removeFromOldBucket(String key, int bucketKey) {
+        if (bucketKey > 0) {
+            Set<String> oldBucket = bucketMap.get(bucketKey);
+            if (oldBucket.size() == 1) {
+                oldBucket.clear();
+                bucketMap.remove(bucketKey);
+            } else {
+                oldBucket.remove(key);
+            }
+        }
+    }
+
+    private void addToNewBucket(String key, int bucketKey) {
+        if (bucketKey > 0) {
+            bucketMap.computeIfAbsent(bucketKey, k -> new HashSet<>()).add(key);
+        }
+    }
+
+    public String getMaxKey() {
+        return bucketMap.isEmpty() ? "" : bucketMap.lastEntry().getValue().iterator().next();
+    }
+    
+    public String getMinKey() {
+        return bucketMap.isEmpty() ? "" : bucketMap.firstEntry().getValue().iterator().next();
+    }
+}
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
