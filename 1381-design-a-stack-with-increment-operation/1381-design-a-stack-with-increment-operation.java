fe4f49def4df5999c8104e59b00754b2da5e4aeb
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-09-30 08:36:15 +0530

    Time: 5 ms (93.47%), Space: 45.2 MB (90.93%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,42 +1,101 @@
-class CustomStack {
+// class CustomStack {
     
-    private int i, arr[];
-    private Stack<Integer> stack;
+//     private int i, arr[];
+//     private Stack<Integer> stack;
+
+//     public CustomStack(int maxSize) {
+//         this.i = maxSize;
+//         this.arr = new int[i];
+//         this.stack = new Stack<Integer>();
+//     }
+    
+//     public void push(int x) {
+//         if(stack.size() < i){
+//             stack.push(x);
+//         }
+//     }
+    
+//     public int pop() {
+//         int j = stack.size() - 1;
+//         if(j < 0){
+//             return -1;
+//         }
+//         if(j>0){
+//             arr[j-1] += arr[j];
+//         }
+//         int res = stack.pop() + arr[j];
+//         arr[j] = 0;
+        
+//         return res;
+//     }
+    
+//     public void increment(int k, int val) {
+//         int j = Math.min(k, stack.size()) - 1;
+//         if(j >= 0){
+//             arr[j] += val;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+class CustomStack {
+
+    // Array to store stack elements
+    private int[] stackArray;
+
+    // Array to store increments for lazy propagation
+    private int[] incrementArray;
+
+    // Current top index of the stack
+    private int topIndex;
 
     public CustomStack(int maxSize) {
-        this.i = maxSize;
-        this.arr = new int[i];
-        this.stack = new Stack<Integer>();
+        stackArray = new int[maxSize];
+        incrementArray = new int[maxSize];
+        topIndex = -1;
     }
-    
+
     public void push(int x) {
-        if(stack.size() < i){
-            stack.push(x);
+        if (topIndex < stackArray.length - 1) {
+            stackArray[++topIndex] = x;
         }
     }
-    
+
     public int pop() {
-        int j = stack.size() - 1;
-        if(j < 0){
+        if (topIndex < 0) {
             return -1;
         }
-        if(j>0){
-            arr[j-1] += arr[j];
+
+        // Calculate the actual value with increment
+        int result = stackArray[topIndex] + incrementArray[topIndex];
+
+        // Propagate the increment to the element below
+        if (topIndex > 0) {
+            incrementArray[topIndex - 1] += incrementArray[topIndex];
         }
-        int res = stack.pop() + arr[j];
-        arr[j] = 0;
-        
-        return res;
+
+        // Reset the increment for this position
+        incrementArray[topIndex] = 0;
+
+        topIndex--;
+        return result;
     }
-    
+
     public void increment(int k, int val) {
-        int j = Math.min(k, stack.size()) - 1;
-        if(j >= 0){
-            arr[j] += val;
+        if (topIndex >= 0) {
+            // Apply increment to the topmost element of the range
+            int incrementIndex = Math.min(topIndex, k - 1);
+            incrementArray[incrementIndex] += val;
         }
     }
 }
-
 /**
  * Your CustomStack object will be instantiated and called as such:
  * CustomStack obj = new CustomStack(maxSize);
