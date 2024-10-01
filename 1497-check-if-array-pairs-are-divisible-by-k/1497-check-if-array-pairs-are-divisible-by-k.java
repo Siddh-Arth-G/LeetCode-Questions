20913e7525c712629e9b293ec2cee12ff30bd96d
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-10-01 11:33:05 +0530

    Time: 4 ms (98.97%), Space: 56.6 MB (52.06%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,38 +1,17 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        HashMap<Integer, Integer> rem = new HashMap<>();
+        int[] freq = new int[k];
         
-        // Count the occurrences of each remainder
-        for (int val : arr) {
-            int re = (val % k + k) % k; // Adjust remainder to be positive
-            int f = rem.getOrDefault(re, 0);
-            rem.put(re, f + 1);
+        for (int num : arr) {
+            freq[((num % k) + k) % k]++;
         }
         
-        // Check conditions for pairing
-        for (int val : arr) {
-            int re = (val % k + k) % k;
-            
-            if (re == 0) { // Remainder 0 should have an even count
-                int fq = rem.get(re);
-                if (fq % 2 != 0) {
-                    return false;
-                }
-            } else if (re == k / 2 && k % 2 == 0) { // Special case when k is even and re == k/2
-                int fq = rem.get(re);
-                if (fq % 2 != 0) {
-                    return false;
-                }
-            } else { // General case: re and (k - re) should have the same count
-                int fq = rem.getOrDefault(re, 0);
-                int ofq = rem.getOrDefault(k - re, 0);
-                if (fq != ofq) {
-                    return false;
-                }
-            }
+        if (freq[0] % 2 != 0) return false;
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) return false;
         }
+        
         return true;
     }
 }
