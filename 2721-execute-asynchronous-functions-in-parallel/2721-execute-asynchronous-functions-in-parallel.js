204a6accde863aaa86532ab8e03fdce07a2fd3b2
Siddh-Arth-G <142684262+Siddh-Arth-G@users.noreply.github.com>
2024-10-28 22:52:04 +0530

    Time: 74 ms (46.83%), Space: 50 MB (27.16%) - LeetHub

--- 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -1,25 +1,25 @@
-/**
- * @param {Array<Function>} functions
- * @return {Promise<any>}
- */
 var promiseAll = async function(functions) {
-    return new Promise((resolve, reject) => {
-        // We know the resulting array will be the same length as functions
-        const results = new Array(functions.length);
-        let count = 0;
-        functions.forEach((fn, i) => {
-            fn()
-            .then(val => {
-                results[i] = val;
-                count++;
-                if(count === functions.length) resolve(results);
-            })
-            .catch(reason => reject(reason));
+    return new Promise((resolve,reject) => {
+        if(functions.length === 0) {
+            resolve([]);
+            return;
+        }
+        
+        const res = new Array(functions.length).fill(null);
+
+        let resolvedCount = 0;
+
+        functions.forEach(async (el,idx) => {
+            try {
+                const subResult = await el();
+                res[idx] = subResult;
+                resolvedCount++;
+                if(resolvedCount=== functions.length) {
+                    resolve(res);
+                }
+            } catch(err) {
+                reject(err);
+            }
         });
     });
 };
-
-/**
- * const promise = promiseAll([() => new Promise(res => res(42))])
- * promise.then(console.log); // [42]
- */
